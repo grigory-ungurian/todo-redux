76000254ffc399f55002e4f146706f27af27e143
Grigory <123222978+grigory-ungurian@users.noreply.github.com>
2023-06-23 04:31:22 +0000

    Update 2 files

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,17 @@
 import { createStore } from "redux";
 import { rootReducer } from "./rootReducer";
+import { loadState, saveState } from "./local-storage";
 
 export const configureStore = () => {
-  return createStore(
+  const persistedState = loadState();
+  const store = createStore(
     rootReducer,
+    persistedState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
+
+  store.subscribe(() => {
+    saveState({ todos: store.getState().todos });
+  });
+  return store;
 };
